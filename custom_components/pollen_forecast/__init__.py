69c5f70a1f332799580657208196d917d7e2c3c0
Chris Stos-Gale <chris.stosgale@epicgames.com>
2024-06-02 18:24:50 +0100

    Update __init__.py

--- custom_components/pollen_forecast/__init__.py
+++ custom_components/pollen_forecast/__init__.py
@@ -6,19 +6,18 @@ from homeassistant.core import HomeAssistant
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
 
 from .sensor import PollenSensor
-import const
 
 CONFIG_SCHEMA = vol.Schema({vol.Optional(CONF_LATITUDE): float, vol.Optional(CONF_LONGITUDE): float}, extra=vol.ALLOW_EXTRA)
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Pollen Sensor component."""
-    hass.data[const.DOMAIN] = {}
-    hass.data[const.DOMAIN][CONF_LATITUDE] = config.get(CONF_LATITUDE, hass.config.latitude)
-    hass.data[const.DOMAIN][CONF_LONGITUDE] = config.get(CONF_LONGITUDE, hass.config.longitude)
+    hass.data[DOMAIN] = {}
+    hass.data[DOMAIN][CONF_LATITUDE] = config.get(CONF_LATITUDE, hass.config.latitude)
+    hass.data[DOMAIN][CONF_LONGITUDE] = config.get(CONF_LONGITUDE, hass.config.longitude)
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Pollen Sensor from a config entry."""
-    hass.data[const.DOMAIN]['sensor'] = PollenSensor(hass.data[const.DOMAIN][CONF_LATITUDE], hass.data[const.DOMAIN][CONF_LONGITUDE])
+    hass.data[DOMAIN]['sensor'] = PollenSensor(hass.data[DOMAIN][CONF_LATITUDE], hass.data[DOMAIN][CONF_LONGITUDE])
     return True
 
